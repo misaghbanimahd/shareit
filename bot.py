a567d997153d113a39359604441274dbfdb5eb3b
misaghbanimahd <misaghfilm@gmail.com>
2022-10-20 00:08:59 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def convert(update,context):
     pattern2="http://*"
     chat="@hamrahmedia"
     user=update.message.from_user.id
-    check=update.getChatMember(chat,user)
+    check="hhhh"
 	
     if check:
     if(re.search(pattern1,link)) or (re.search(pattern2,link)):
