8bf08e46b11f3ef0967c886adb6cf59d03d6b8f9
misaghbanimahd <misaghfilm@gmail.com>
2022-10-19 21:36:00 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters,ConversationHandler,CallbackQueryHandler,InlineQueryHandler
 import telegram
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,InputTextMessageContent,InlineQueryResultArticle
+from telegram.apihelper import ApiTelegramException
 import logging
 import os
 import unshortenit
