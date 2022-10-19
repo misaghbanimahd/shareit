66c454f544e404ff93959f3d75f37c14d0e4d1f4
misaghbanimahd <misaghfilm@gmail.com>
2022-10-20 01:04:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,12 +39,15 @@ def convert(update,context):
     chat="@hamrahmedia"
     user=update.message.from_user.id
     check=context.bot.getChatMember(chat,user)
-if check.status in ['member','administrator']:
+if check.status=="administrator":
 	
 	
+         if(re.search(pattern1,link)) or (re.search(pattern2,link)):
         keyboard = [[InlineKeyboardButton("Short", callback_data='short'),InlineKeyboardButton("Unshort", callback_data='unshort')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text('Select from below options whether you want to short or unshort your url', reply_markup=reply_markup)
+        else:
+        update.message.reply_text("<i>⚠️ Url must start with http:// or https:// and it should not have spaces in it.</i>",parse_mode=telegram.ParseMode.HTML)
        
     
 else:
